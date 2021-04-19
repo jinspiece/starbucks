cd0f3d15f0e0c11db3f463cb011aa184ae238387
jinspiece <jinspiece@gmail.com>
2021-04-20 00:28:32 +0900

    공통 모듈 분리

---
+++ js/common.js
@@ -0,0 +1,21 @@
+// search meterial icon focus
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+// 연도 자동 갱신 설정
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
